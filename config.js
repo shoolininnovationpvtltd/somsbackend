7d69ae33c1c6d521d5f3a7c6e77e4330e988c289
school of maritime studies <developer@shoolin.co.uk>
2024-09-06 20:34:41 +0530

    ksdj

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
 const mongoose = require('mongoose');
-mongoose.connect("mongodb+srv://developer:bDoi6siv1pyRpRZz@somsmaritime.qpkfy.mongodb.net/?retryWrites=true&w=majority&appName=somsmaritime");
+mongoose.connect(process.env.MONGO_URL);
